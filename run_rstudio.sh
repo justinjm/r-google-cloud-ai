0e59d4730dec7e282ae7108e11ee7237bf8243b8
Justin Marciszewski <justinjm@google.com>
2022-12-16 13:26:04 -0500

    replacing hardcoded project id

--- run_rstudio.sh
+++ run_rstudio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
-docker pull us-central1-docker.pkg.dev/demos-vertex-ai/rstudio/rstudio:latest
-docker run -d -p 8080:8787 us-central1-docker.pkg.dev/demos-vertex-ai/rstudio/rstudio 
+docker pull us-central1-docker.pkg.dev/$DEVSHELL_PROJECT_ID/rstudio/rstudio:latest
+docker run -d -p 8080:8787 us-central1-docker.pkg.dev/$DEVSHELL_PROJECT_ID/rstudio/rstudio 
 echo https://8080-$WEB_HOST
